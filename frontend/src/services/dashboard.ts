6c779705c2dc372e5100ac934c1d6b7cfecaf43a
ddusichka <78527291+ddusichka@users.noreply.github.com>
2025-01-07 22:33:24 -0500

    wrap in try/catch

--- frontend/src/services/dashboard.ts
+++ frontend/src/services/dashboard.ts
@@ -1,14 +1,19 @@
 import { Item } from "../types";
 
 export async function fetchDashboardItems(): Promise<Item[]> {
-  // real API call would go here. for now, mock data
-  return [
-    { name: "Lumber", description: "Lumber from trees", price: 5000 },
-    {
-      name: "Shipping",
-      description: "Shipping lumber cross-country",
-      price: 1000,
-    },
-    { name: "Paint", description: "Paint", price: 1500 },
-  ];
+  try {
+    // real API call would go here. for now, mock data
+    return [
+      { name: "Lumber", description: "Lumber from trees", price: 5000 },
+      {
+        name: "Shipping",
+        description: "Shipping lumber cross-country",
+        price: 1000,
+      },
+      { name: "Paint", description: "Paint", price: 1500 },
+    ];
+  } catch (error) {
+    console.error("Error fetching dashboard items", error);
+    return [];
+  }
 }
