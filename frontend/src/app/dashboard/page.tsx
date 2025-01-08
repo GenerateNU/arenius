6b975618039045e72a5ec5e959606a5638e5c8d4
Dessy Dusichka <78527291+ddusichka@users.noreply.github.com>
2025-01-07 22:41:42 -0500

    feat: init frontend (#5)

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import ItemTable from "@/components/dashboard/ItemTable";
+
+export default function Dashboard() {
+  return (
+    <div className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <ItemTable />
+    </div>
+  );
+}
---
+++ frontend/src/components/dashboard/ItemRow.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Item } from "@/types";
+
+type ItemTableRowProps = Item;
+
+export default function ItemTableRow({
+  name,
+  description,
+  price,
+}: ItemTableRowProps) {
+  return (
+    <div className={styles.container}>
+      <p>{name}</p>
+      <p>{description}</p>
+      <p>{price}</p>
+    </div>
+  );
+}
+
+const styles = {
+  container: "grid grid-cols-3 gap-2",
+};
