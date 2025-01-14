7abfcf350e2163aac876dab7391e2ab476623429
Adam Ma <adam.zym@gmail.com>
2025-01-13 20:04:40 -0500

    Create Line Item Table

---
+++ backend/internal/supabase/migrations/20250114003404_create_line_item_table.sql
@@ -0,0 +1,19 @@
+create table line_item (
+    id int not null primary key,
+    xero_line_item_id uuid,
+    description text not null,
+    quantity float not null,
+    unit_amount float not null,
+    company_id uuid not null,
+    contact_id uuid not null,
+    date timestamp not null,
+    currency_code text not null,
+
+    emission_factor text,
+    amount float,
+    unit text,
+    co2 float,
+    scope int,
+
+    foreign key (company_id) references company(id)
+);
