217db3d13984700763a66adfc9265ac9ca3227dc
Luisa <103768169+luisa-li@users.noreply.github.com>
2025-01-27 01:17:20 -0500

    blurb

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import ItemTable from "@/components/dashboard/ItemTable";
 import ItemForm from "@/components/dashboard/ItemForm";
 
+// TODO: UPDATING THE FORM DOESN'T ACTUALLY CHANGE THE TABLE LIVE, AND IM NOT SURE HOW TO DO THAT
 export default function Dashboard() {
   return (
     <div className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
