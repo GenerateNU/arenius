6edfe142eed45c1525dfcc29879085451394afb3
Dessy Dusichka <78527291+ddusichka@users.noreply.github.com>
2025-01-28 18:08:45 -0500

    Update line items screen (#47)

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -25,7 +25,11 @@ export default function Dashboard() {
   return (
     <div className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <ItemTable items={data} />
-      <ItemForm onSubmit={addItem} />
+      <hr className="my-8 border border-black-100" />
+      <div>
+        <p className="font-bold text-xl">Add a new line item</p>
+        <ItemForm onSubmit={addItem} />
+      </div>
     </div>
   );
 }
