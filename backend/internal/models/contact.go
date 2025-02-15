468f47e3cf90821fd5f813c85088f15b586db156
Adam Ma <adam.zym@gmail.com>
2025-02-15 13:01:12 -0500

    Formatting

--- backend/internal/models/contact.go
+++ backend/internal/models/contact.go
@@ -1,12 +1,12 @@
 package models
 
 type Contact struct {
-	ID             string     `json:"id"`
-	Name           string     `json:"name"`
-	Email		   string	  `json:"email"`
-	Phone		   string	  `json:"phone"`
-	City		   string	  `json:"city"`
-	State	   	   string	  `json:"state"`
-	XeroContactID  *string    `json:"xero_contact_id,omitempty"`
-	CompanyID	   string	  `json:"company_id"`
+	ID            string  `json:"id"`
+	Name          string  `json:"name"`
+	Email         string  `json:"email"`
+	Phone         string  `json:"phone"`
+	City          string  `json:"city"`
+	State         string  `json:"state"`
+	XeroContactID *string `json:"xero_contact_id,omitempty"`
+	CompanyID     string  `json:"company_id"`
 }
