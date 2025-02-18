64da69b9e05c2faf2a2cdf29f2f9f637cc00676a
Ira Hysi <irahysi2024@gmail.com>
2025-02-18 00:29:34 -0500

    nav bar

--- frontend/src/app/all_navs/transactions/page.tsx
+++ frontend/src/app/all_navs/transactions/page.tsx
@@ -6,15 +6,14 @@ import { fetchLineItems } from "@/services/lineItems";
 import { LineItem } from "@/types";
 import LineItemTable from "@/components/transactions/LineItemTable"
 import { columns} from "@/components/transactions/columns";
+import { LineItemFilters } from "@/types";
 
 export default function Transactions() {
   const [data, setData] = useState<LineItem[]>([]);
 
-  // eslint-disable-next-line 
   const getItems = async () => {
-    // eslint-disable-next-line
-    const items = await fetchLineItems();
-    // eslint-disable-next-line
+    const filters: LineItemFilters = { /* define your filters here */ };
+    const items = await fetchLineItems(filters);
     setData(items);
   };
 
@@ -26,12 +25,12 @@ export default function Transactions() {
     <div className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] flex-1">
       <div>
         <p className="font-bold text-xl">Add a new line item</p>
-        {/* eslint-disable-next-line */}
+        {/* // eslint-disable-next-line */}
         <ItemForm handleSubmit={getItems} />
       </div>
       <hr className="mb-4 border border-black-100" />
 
-      {/* eslint-disable-next-line */}
+      {/* // eslint-disable-next-line */}
       <LineItemTable columns={columns} data={data} getRowId={(row) => row.id} onReconcile={getItems} />
 
     </div>
