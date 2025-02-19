c1730e6c00246512db2dad0c74429dfaa54ba1e9
ddusichka <78527291+ddusichka@users.noreply.github.com>
2025-02-19 13:18:54 -0500

    use .env for auth redirect

--- frontend/src/app/welcome/page.tsx
+++ frontend/src/app/welcome/page.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import ImageStack from "@/components/onboarding/onboarding-nav";
+import Link from "next/link";
 
 const WelcomeMessage: React.FC = () => {
   return (
     <div className="flex h-screen w-full">
       <div className="w-1/3 flex flex-col items-center justify-center p-8 bg-[#F8F8F8]">
-        <ImageStack atXero={true} atWelcome={true}/>
+        <ImageStack atXero={true} atWelcome={true} />
       </div>
 
       <div className="w-2/3 flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold text-gray-900 mb-4">
           All done! Welcome to Arenius!
         </h1>
-        <button className="w-40 h-10 bg-[#59C295] text-white text-sm font-semibold rounded-md">
-          Go To Dashboard
-        </button>
+        <Link href="/transactions">
+          <button className="w-40 h-10 bg-[#59C295] text-white text-sm font-semibold rounded-md">
+            Go to dashboard
+          </button>
+        </Link>
       </div>
     </div>
   );
