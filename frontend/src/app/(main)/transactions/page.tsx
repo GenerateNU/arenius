6a6ac480312f2d28b8edce6229f3fdeebf47d2cc
ddusichka <78527291+ddusichka@users.noreply.github.com>
2025-02-19 22:44:52 -0500

    invisible route group

---
+++ frontend/src/app/(main)/transactions/page.tsx
@@ -0,0 +1,30 @@
+import ItemForm from "@/components/transactions/ItemForm";
+import LineItemTable from "@/components/transactions/LineItemTable";
+import LineItemTableFilters from "@/components/transactions/LineItemTableFilters";
+import { LineItemsProvider } from "@/context/LineItemsContext";
+
+export default function Transactions() {
+  return (
+    <LineItemsProvider>
+      <div className={styles.container}>
+        <div>
+          <p className={styles.formTitle}>Add a new line item</p>
+          <ItemForm />
+        </div>
+        <hr className={styles.spacer} />
+        <div>
+          <p className={styles.formTitle}>Transactions</p>
+          <LineItemTableFilters />
+          <LineItemTable />
+        </div>
+      </div>
+    </LineItemsProvider>
+  );
+}
+
+const styles = {
+  container:
+    "p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] flex-1",
+  formTitle: "font-bold text-xl",
+  spacer: "mb-4 border border-black-100",
+};
