9e0f1e504f2e070bc1c254ce8e3a4c3c703eacd4
adescoteaux1 <ally.descoteaux.0@gmail.com>
2025-03-10 22:09:54 -0400

    remove hardcode

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://sea-lion-app-y4r7v.ondigitalocean.app/arenius-backend2",//process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
