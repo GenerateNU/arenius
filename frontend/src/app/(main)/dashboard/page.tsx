7635cd009fc973f0f7e10d3ad38cfae08390977b
adescoteaux1 <ally.descoteaux.0@gmail.com>
2025-03-11 19:53:31 -0400

    start

--- frontend/src/app/(main)/dashboard/page.tsx
+++ frontend/src/app/(main)/dashboard/page.tsx
@@ -1,11 +1,18 @@
 // app/contacts/page.tsx
+"use client";
 
 import React from "react";
+import dynamic from "next/dynamic";
+
+const ApexChart = dynamic(() => import("@/components/dashboard/treemap"), {
+  ssr: false, // Ensures it only renders on the client
+});
 
 const ContactsPage: React.FC = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <h1 className="text-3xl font-bold">Dashboard Page</h1>
+      <ApexChart />
     </div>
   );
 };
