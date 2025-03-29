ad05cbcee86c6b19856e6204f84be77bae1a8bac
Ira Hysi <irahysi2024@gmail.com>
2025-03-29 17:31:15 -0400

    s3

--- frontend/src/app/(main)/profile/page.tsx
+++ frontend/src/app/(main)/profile/page.tsx
@@ -5,6 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
+console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL or ANON KEY is missing!");
 }
