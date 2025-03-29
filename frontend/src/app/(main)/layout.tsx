774ff56bf600fe1badca6eea8e7d8dd2202033d8
Michael Brennan <76018881+michael-brennan2005@users.noreply.github.com>
2025-03-29 18:05:48 -0400

    Nav bar bug (#190)

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,22 +1,25 @@
 "use client";
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 import Image from "next/image";
 import onboardingLogo from "@/assets/onboarding-logo.png";
 import Link from "next/link";
 import DashboardIcon from "@/components/icons/dashboard";
 import TransactionsIcon from "@/components/icons/transactions";
 import ContactsIcon from "@/components/icons/contacts";
+import { usePathname } from "next/navigation";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [activeTab, setActiveTab] = useState<string>("transactions");
+  const activeTab = usePathname();
 
-  const handleTabClick = (tab: string) => {
-    setActiveTab(tab);
-  };
+  const links = [
+    { href: "/dashboard", label: "Dashboard", icon: DashboardIcon},
+    { href: "/transactions", label: "Transactions", icon: TransactionsIcon},
+    { href: "/contacts", label: "Contacts", icon: ContactsIcon}
+  ];
 
   return (
     <div className="relative overflow-x-hidden flex-1">
@@ -30,42 +33,20 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           />
         </div>
         <div className="flex justify-end w-full max-w-full p-4 space-x-4">
-          <div className="z-1">
-            <DashboardIcon activeTab={activeTab} />
-          </div>
-          <Link
-            href="/dashboard"
-            className={`text-lg font-bold ${
-              activeTab === "dashboard" ? "text-primary" : "text-gray-700"
-            } pr-5`}
-            onClick={() => handleTabClick("dashboard")}
-          >
-            Dashboard
-          </Link>
-          <div className="z-1">
-            <TransactionsIcon activeTab={activeTab} />
-          </div>
-          <Link
-            href="/transactions"
-            className={`text-lg font-bold ${
-              activeTab === "transactions" ? "text-primary" : "text-gray-700"
-            } pr-5`}
-            onClick={() => handleTabClick("transactions")}
-          >
-            Transactions
-          </Link>
-          <div className="z-1">
-            <ContactsIcon activeTab={activeTab} />
-          </div>
-          <Link
-            href="/contacts"
-            className={`text-lg font-bold ${
-              activeTab === "contacts" ? "text-primary" : "text-gray-700"
-            } pr-5`}
-            onClick={() => handleTabClick("contacts")}
-          >
-            Contacts
-          </Link>
+          {links.map((val) => 
+            <>
+              <div className="z-1">
+                <val.icon active={activeTab === val.href}/>
+              </div>
+              <Link
+                href={val.href}
+                className={`text-lg font-bold ${
+                  activeTab === val.href ? "text-primary" : "text-gray-700"
+                } pr-5`}>
+                {val.label}
+              </Link>
+            </>
+          )}
         </div>
       </div>
       <div className="pt-10 px-20 bg-grayBackground h-full">{children}</div>{" "}
