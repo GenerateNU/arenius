a58f78fe150a2530b708de19170df3d06fa108aa
Ira Hysi <irahysi2024@gmail.com>
2025-03-29 17:25:46 -0400

    s3

--- frontend/src/app/(main)/profile/page.tsx
+++ frontend/src/app/(main)/profile/page.tsx
@@ -2,11 +2,14 @@
 import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL or ANON KEY is missing!");
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function ProfilePage() {
   const [isUploading, setIsUploading] = useState(false);
