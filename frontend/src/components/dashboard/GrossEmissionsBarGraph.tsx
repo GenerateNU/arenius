e0cecd3e5e58767aa4a6b40552d0ab6fa0e0a696
ddusichka <78527291+ddusichka@users.noreply.github.com>
2025-04-01 10:42:17 -0400

    update gross emissions styling

--- frontend/src/components/dashboard/GrossEmissionsBarGraph.tsx
+++ frontend/src/components/dashboard/GrossEmissionsBarGraph.tsx
@@ -28,15 +28,15 @@ import { MonthSummary } from "@/types";
 const chartConfig = {
   scope1: {
     label: "Scope 1",
-    color: "rgba(48,100,68,255)",
+    color: "rgb(204,238,186)",
   },
   scope2: {
     label: "Scope 2",
-    color: "rgba(89,194,114,255)",
+    color: "rgb(112,158,92)",
   },
   scope3: {
     label: "Scope 3",
-    color: "rgba(180,242,171,255)",
+    color: "rgb(40,59,24)",
   },
 } satisfies ChartConfig;
 
@@ -70,16 +70,24 @@ export default function GrossEmissionsBarGraph() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle style={{ fontSize: "1.5rem", marginBottom: 8 }}>
-          Gross Emissions
-        </CardTitle>
-        <CardDescription style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
-          {grossSummary.total_co2?.toLocaleString("en-US") || 0} kg
-        </CardDescription>
-        <CardDescription>
-          Total emissions (kg) for <span className="font-bold">{formattedStartMonth} {formattedStartYear} -{" "}
-          {formattedEndMonth} {formattedEndYear}</span>
-        </CardDescription>
+        <div className="flex justify-between font-[Arimo]">
+          <div>
+            <CardTitle className="text-black text-4xl mb-2 font-semibold">
+              Gross Emissions
+            </CardTitle>
+            <CardDescription className="text-black text-4xl font-semibold font-[Arimo]">
+              {grossSummary.total_co2?.toLocaleString("en-US") || 0} kg
+            </CardDescription>
+            <CardDescription className="font-[Montserrat] py-2">
+              Total emissions (kg) for{" "}
+              <span className="font-bold">
+                {formattedStartMonth} {formattedStartYear} – {formattedEndMonth}{" "}
+                {formattedEndYear}
+              </span>
+            </CardDescription>
+          </div>
+          <CustomLegend />
+        </div>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
@@ -87,37 +95,36 @@ export default function GrossEmissionsBarGraph() {
             <BarChart accessibilityLayer data={chartData}>
               <defs>
                 <linearGradient id="scope1Gradient" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="0%" stopColor="#5F8D39" stopOpacity={1}/>
-                  <stop offset="100%" stopColor="#2B3E1B" stopOpacity={1}/>
+                  <stop offset="0%" stopColor="#D0F5BC" stopOpacity={1} />
+                  <stop offset="100%" stopColor="#C3DCB5" stopOpacity={1} />
                 </linearGradient>
                 <linearGradient id="scope2Gradient" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="0%" stopColor="#94BE7C" stopOpacity={1}/>
-                  <stop offset="39%" stopColor="#5C8957" stopOpacity={1}/>
-                  <stop offset="100%" stopColor="#36663F" stopOpacity={1}/>
+                  <stop offset="0%" stopColor="#ACC99B" stopOpacity={1} />
+                  <stop offset="100%" stopColor="#276E0B" stopOpacity={1} />
                 </linearGradient>
                 <linearGradient id="scope3Gradient" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="0%" stopColor="#D0F5BC" stopOpacity={1}/>
-                  <stop offset="100%" stopColor="#77B257" stopOpacity={1}/>
+                  <stop offset="0%" stopColor="#426227" stopOpacity={1} />
+                  <stop offset="100%" stopColor="#0A0F06" stopOpacity={1} />
                 </linearGradient>
               </defs>
-              <CartesianGrid vertical={false} />
+              <CartesianGrid vertical={false} strokeDasharray="4 4" />
               <XAxis
                 dataKey="month"
                 tickLine={true}
                 tickMargin={10}
                 axisLine={false}
+                fontFamily="Montserrat"
                 tickFormatter={(value) => value.slice(0, 3)}
               />
-              <YAxis />
+              <YAxis axisLine={false} fontFamily="Montserrat" />
               <ChartTooltip
                 content={<ChartTooltipContent hideLabel />}
                 wrapperStyle={{ width: "12%" }}
               />
-              {/* <ChartLegend content={<ChartLegendContent />} /> */}
               <Bar
-                dataKey="scope1"
+                dataKey="scope3"
                 stackId="a"
-                fill="url(#scope1Gradient)"
+                fill="url(#scope3Gradient)"
                 radius={12}
               />
               <Bar
@@ -127,9 +134,9 @@ export default function GrossEmissionsBarGraph() {
                 radius={12}
               />
               <Bar
-                dataKey="scope3"
+                dataKey="scope1"
                 stackId="a"
-                fill="url(#scope3Gradient)"
+                fill="url(#scope1Gradient)"
                 radius={12}
               />
             </BarChart>
@@ -139,3 +146,21 @@ export default function GrossEmissionsBarGraph() {
     </Card>
   );
 }
+
+const CustomLegend = () => {
+  return (
+    <div className="flex flex-col gap-2 mt-4">
+      {Object.entries(chartConfig).map(([key, { label, color }]) => (
+        <div key={key} className="flex items-center gap-2">
+          <span
+            className="w-4 h-4 rounded-full"
+            style={{ backgroundColor: color }}
+          />
+          <span className="text-md text-muted-foreground font-[Montserrat]">
+            {label}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
