9aec958ddb5c3572b26daf9d4c148e610c67a6a1
Ira Hysi <irahysi2024@gmail.com>
2025-04-05 16:55:59 -0400

    remember me

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import LoginForm from "@/components/onboarding/login-form";
 import SignupForm from "@/components/onboarding/landing";
+import Cookies from "js-cookie";
 
 export default function LoginPage() {
+  const router = useRouter();
+
   const [isLoginPage, setIsLoginPage] = useState(false);
+
+  const jwt = Cookies.get("jwt")
+  const userId = Cookies.get("userID")
+
+  useEffect(() => {
+    if (jwt && userId) {
+      router.push("/dashboard"); 
+    }
+  }, [jwt, userId, router]);
+
 
   return (
     <div className={styles.container}>
