9a07f320d421bb5e012ec02210bcdb2d19ed18f4
jtoncelli <jack.toncelli@gmail.com>
2025-04-07 17:33:25 -0400

    added PATCH to contact/:contactId

---
+++ backend/internal/service/handler/contact/update_contacts.go
@@ -0,0 +1,26 @@
+package contact
+
+import (
+	"arenius/internal/errs"
+	"arenius/internal/models"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h *Handler) UpdateContact(c *fiber.Ctx) error {
+	contactId := c.Params("contactId")
+
+	var req models.UpdateContactRequest
+	if err := c.BodyParser(&req); err != nil {
+		return errs.BadRequest(fmt.Sprintf("error parsing request body: %v", err))
+	}
+
+	// TODO: change t
+	contact, err := h.contactRepository.UpdateContact(c.Context(), contactId, req)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(contact)
+}
