123ddc187af821d2e224a87afe29fb762e0725db
Ally Descoteaux <128718372+adescoteaux1@users.noreply.github.com>
2025-04-08 20:35:22 -0400

    tooltip (#250)
    Co-authored-by: adescoteaux1 <ally.descoteaux.0@gmail.com>

--- frontend/src/components/transactions/UnreconciledView.tsx
+++ frontend/src/components/transactions/UnreconciledView.tsx
@@ -1,20 +1,34 @@
 import React from "react";
 import LineItemTable from "./LineItemTable";
 import { recommendationColumns, unreconciledColumns } from "./columns";
+import { Tooltip } from "../ui/tooltip";
+import { HelpCircle } from "lucide-react";
+import { textConstants } from "@/lib/utils";
 
 const UnreconciledView = () => {
   return (
     <div>
-      <p className="text-xl font-bold py-2">Suggested reconciliations</p>
+      <div className="flex items-center">
+        <p className="text-xl font-bold py-2 mr-2">Suggested Reconciliations</p>
+        <Tooltip textContent={textConstants.recommendation}>
+          <HelpCircle className="w-4" />
+        </Tooltip>
+      </div>
       <LineItemTable
         activePage={"unreconciled"}
         activeTableData="recommended"
         columns={recommendationColumns}
         paginated={false}
       />
-      <p className="text-xl font-bold mt-6 py-2">
-        All unreconciled reconciliations
-      </p>
+      <div className="flex items-end">
+        <p className="text-xl font-bold mt-6 py-2 mr-2">
+          All Unreconciled Reconciliations
+        </p>
+        <Tooltip textContent={textConstants.unreconciled}>
+          <HelpCircle className="w-4 mb-3" />
+        </Tooltip>
+      </div>
+      
 
       <LineItemTable
         activePage={"unreconciled"}
