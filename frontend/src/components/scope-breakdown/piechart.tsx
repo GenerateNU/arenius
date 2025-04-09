ca34a986d294369869bf9ec041955bba0a1cc0f5
Adam Ma <adam.zym@gmail.com>
2025-04-09 18:39:52 -0400

    See Previous Commit Msg

--- frontend/src/components/scope-breakdown/piechart.tsx
+++ frontend/src/components/scope-breakdown/piechart.tsx
@@ -27,12 +27,12 @@ const ScopeChart: React.FC<{
         data={chartData}
         dataKey="value"
         nameKey="name"
-        innerRadius={90} // Creates donut effect
+        innerRadius={95} // Creates donut effect
         outerRadius={120}
-        paddingAngle={3}
+        paddingAngle={2}
         stroke="#fff"
         strokeWidth={2}
-        cornerRadius={36}
+        cornerRadius={6}
         labelLine={false}
       >
         {chartData.map((_, index) => (
